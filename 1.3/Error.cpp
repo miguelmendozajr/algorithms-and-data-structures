0457800e8317952aa10f2c6caae9d5458f19d6be
Miguel Mendoza Jaidar <117315115+miguelmendozajr@users.noreply.github.com>
2023-09-05 14:02:17 -0600

    Create Error.cpp

---
+++ 1.3/Error.cpp
@@ -0,0 +1,104 @@
+//Gabriel Ernesto Mujica Proulx
+//A01285409
+//01/09/2023
+//Implementacion clase Error
+
+#ifndef ERROR_CPP
+#define ERROR_CPP
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+Error::Error(){
+	mes = 1;
+	dia = 1;
+	hora = "-";
+	IP = "-";
+	razon = "-";
+}
+
+Error::Error(string _mes, int _dia, string _hora, string _IP, string _razon){
+	if(_mes == "Jan"){
+		mes == 1;
+	}
+	if(_mes == "Feb"){
+		mes == 2;
+	}
+	if(_mes == "Mar"){
+		mes == 3;
+	}
+	if(_mes == "Apr"){
+		mes == 4;
+	}
+	if(_mes == "May"){
+		mes == 5;
+	}
+	if(_mes == "Jun"){
+		mes == 6;
+	}
+	if(_mes == "Jul"){
+		mes == 7;
+	}
+	if(_mes == "Aug"){
+		mes == 8;
+	}
+	if(_mes == "Sep"){
+		mes == 9;
+	}
+	if(_mes == "Oct"){
+		mes == 10;
+	}
+	if(_mes == "Nov"){
+		mes == 11;
+	}
+	if(_mes == "Dec"){
+		mes == 12;
+	}
+	dia = _dia;
+	hora = _hora;
+	IP = _IP;
+	razon = _razon;
+}
+
+
+void Error::setMes(string _mes){
+
+}
+
+void Error::setDia(int _dia){
+
+}
+
+void Error::setHora(string _hora){
+
+}
+
+void Error::setIP(string _IP){
+
+}
+
+void Error::setRazon(string _razon){
+
+}
+
+
+int Error::getMes(){
+
+}
+
+int Error::getDia(){
+
+}
+
+string Error::getHora(){
+
+}
+
+string Error::getIP(){
+
+}
+
+string Error::getRazon(){
+
+}
