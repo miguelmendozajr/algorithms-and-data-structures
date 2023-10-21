b5b941d8fa6bc00a6dc1f513e38890d1d04e67a7
Pansocrates03 <87460697+Pansocrates03@users.noreply.github.com>
2023-10-21 10:24:25 -0600

    Add files via upload

---
+++ 2.3/Error.h
@@ -0,0 +1,67 @@
+// Gabriel Ernesto Mujica Proulx A01285409
+// Miguel Mendoza Jaidar A01234354
+// Esteban Sierra Baccio A00836286
+
+// Declaración y definición de la clase Error
+// Ultima modificacion 07/09/2023
+
+#include <string>
+#include "Date.h"
+
+using namespace std;
+
+class Error{
+	private:
+        Date fecha;
+		string IP;
+		string razon;
+
+	public:
+		Error();
+		Error(Date _fecha, string _IP, string _razon);
+
+		void setIP(string _IP);
+		void setRazon(string _razon);
+        void setFecha(Date _fecha);
+
+		string getIP();
+		string getRazon();
+        Date getFecha();
+
+};
+
+Error::Error(){
+    Date fecha;
+	IP = "-";
+	razon = "-";
+}
+
+Error::Error(Date _fecha, string _IP, string _razon){
+    fecha = _fecha;
+	IP = _IP;
+	razon = _razon;
+}
+
+void Error::setIP(string _IP){
+    IP = _IP;
+}
+
+void Error::setRazon(string _razon){
+    razon = _razon;
+}
+
+void Error::setFecha(Date _fecha){
+    fecha = _fecha;
+}
+
+string Error::getIP(){
+    return IP;
+}
+
+string Error::getRazon(){
+	return razon;
+}
+
+Date Error::getFecha(){
+    return fecha;
+}
