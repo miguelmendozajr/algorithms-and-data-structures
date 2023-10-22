cc71b3f51350fa23a453253c267e70cca30e8250
“miguelmendozajr” <miguelmendozaj20@gmail.com>
2023-10-21 18:54:33 -0600

    simplified node class

---
+++ 2.3/Node.h
@@ -0,0 +1,21 @@
+#ifndef NODE_H
+#define NODE_H
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node* next;
+    Node* prev;
+
+    Node(T data);
+};
+
+template <typename T>
+Node<T>::Node(T data) {
+    this->data = data;
+    this->next = nullptr;
+    this->prev = nullptr;
+}
+
+#endif
