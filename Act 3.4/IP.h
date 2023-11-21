fcbef3bbe070a968b98a20511c6b24cddc60ee76
gmujicap <110833843+gmujicap@users.noreply.github.com>
2023-11-21 17:50:18 -0600

    IP Final

--- Act 3.4/IP.h
+++ Act 3.4/IP.h
@@ -1,6 +1,10 @@
-// Esteban Sierra Baccio
-// Declaración y definición de la clase IP
-// Ultima modificacion 14/nov/2023
+// Gabriel Ernesto Mujica Proulx A01285409
+// Miguel Mendoza Jaidar A01234354
+// Esteban Sierra Baccio A00836286
+
+// Actividad 3.4
+// Ultima modificacion 21/nov/2023
+// Declaracion e Implementacion clase IP
 
 #include <iostream>
 #include <cstdlib>
@@ -32,19 +36,19 @@ class IP{
 		int num2;
 		int num3;
 		int num4;
-		int port;
 
 	public:
 		IP(),
 		IP(string _IP);
+		IP(string _IP, int _key);
 
 		void setIP(string _IP);
-
 		string getIP();
 
 		bool operator > (IP _ip);
 		bool operator < (IP _ip);
 		bool operator == (IP _ip);
+		bool operator != (IP _ip);
 
 };
 
@@ -53,33 +57,37 @@ IP::IP(){
 	num2 = 0;
 	num3 = 0;
 	num4 = 0;
-	port = 0;
 }
 
 IP::IP(string _IP){
-	vector<string> results, results2;
+	vector<string> results;
 	results = split(_IP, '.');
-	results2 = split(_IP, ':');
 	num1 = stoi(results[0]);
 	num2 = stoi(results[1]);
 	num3 = stoi(results[2]);
 	num4 = stoi(results[3]);
-	port = stoi(results2[1]);
+}
+
+IP::IP(string _IP, int _key){
+	vector<string> results;
+	results = split(_IP, '.');
+	num1 = stoi(results[0]);
+	num2 = stoi(results[1]);
+	num3 = stoi(results[2]);
+	num4 = stoi(results[3]);
 }
 
 void IP::setIP(string _IP){
-	vector<string> results, results2;
+	vector<string> results;
 	results = split(_IP, '.');
-	results2 = split(_IP, ':');
 	num1 = stoi(results[0]);
 	num2 = stoi(results[1]);
 	num3 = stoi(results[2]);
 	num4 = stoi(results[3]);
-	port = stoi(results2[1]);
 }
 
 string IP::getIP(){
-	return to_string(num1) + "." + to_string(num2) + "." + to_string(num3) + "." + to_string(num4) + ":" + to_string(port);
+	return to_string(num1) + "." + to_string(num2) + "." + to_string(num3) + "." + to_string(num4);
 }
 
 bool IP::operator>(IP _ip){
@@ -94,11 +102,7 @@ bool IP::operator>(IP _ip){
 			else if (num3 == _ip.num3){
 				if(num4 > _ip.num4){return true;}
 				else if (num4 < _ip.num4){return false;}
-				else if (num4 == _ip.num4){
-					if(port > _ip.port){return true;}
-					else if (port < _ip.port){return false;}
-					else if (port == _ip.port){return false;}
-				}
+				else if (num4 == _ip.num4){return false;}
 			}
 		}
 	}
@@ -120,19 +124,25 @@ bool IP::operator<(IP _ip){
 			else if (num3 == _ip.num3){
 				if(num4 > _ip.num4){return false;}
 				else if (num4 < _ip.num4){return true;}
-				else if (num4 == _ip.num4){
-					if(port > _ip.port){return false;}
-					else if (port < _ip.port){return true;}
-					else if (port == _ip.port){return false;}
-				}
+				else if (num4 == _ip.num4){return false;}
 			}
 		}
 	}
 	return false;
 }
 
+
 bool IP::operator==(IP _ip){
-	if(num1 == _ip.num1 && num2 == _ip.num2 && num3 == _ip.num3 && num4 == _ip.num4 && port == _ip.port){
+	if(num1 == _ip.num1 && num2 == _ip.num2 && num3 == _ip.num3 && num4 == _ip.num4){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+bool IP::operator!=(IP _ip){
+	if(num1 != _ip.num1 || num2 != _ip.num2 || num3 != _ip.num3 || num4 != _ip.num4){
 		return true;
 	}
 	else{
